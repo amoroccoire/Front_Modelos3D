926fcd713385ab17ad827df6393bd4bcc801d130
amoroccoire <amoroccoire@unsa.edu.pe>
2024-12-15 14:12:33 -0500

    visualizador

--- src/dashboard/viewer-3d.tsx
+++ src/dashboard/viewer-3d.tsx
@@ -3,11 +3,13 @@ import { OrbitControls, Stage } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { Box } from '@mui/material';
 import { Model } from './model';
+import { TextGeometry } from 'three/examples/jsm/Addons.js';
 interface Viewer3DProps {
   modelUrl: string;
 }
 
 export function Viewer3D({ modelUrl }: Viewer3DProps) {
+  const isZipFile = modelUrl.endsWith(".zip");
   return (
     <Box
       sx={{
@@ -21,21 +23,29 @@ export function Viewer3D({ modelUrl }: Viewer3DProps) {
         bgcolor: 'transparent', // Fondo blanco para el área del visor
       }}
     >
-      <Canvas shadows camera={{ position: [0, 0, 4], fov: 50 }} style = {{position: 'absolute', zIndex: 0,}}>
-        <Suspense fallback={null}>
-          <Stage environment="studio" intensity={0.5}>
-            <Model url={modelUrl} />
-          </Stage>
-        </Suspense>
-        <OrbitControls
-          autoRotate
-          autoRotateSpeed={0.5}
-          enableZoom={true}
-          enablePan={true}
-          minPolarAngle={Math.PI / 4}
-          maxPolarAngle={Math.PI / 1.5}
-        />
-      </Canvas>
+      {
+        isZipFile ? (
+          <Model zipUrl={modelUrl}/>
+        ) : (
+          <Canvas shadows camera={{ position: [0, 0, 4], fov: 50 }} style = {{position: 'absolute', zIndex: 0,}}>
+            <Suspense fallback={null}>
+              <Stage environment="studio" intensity={0.5}>
+                <mesh>
+                  
+                </mesh>
+              </Stage>
+            </Suspense>
+            <OrbitControls
+              autoRotate
+              autoRotateSpeed={0.5}
+              enableZoom={true}
+              enablePan={true}
+              minPolarAngle={Math.PI / 4}
+              maxPolarAngle={Math.PI / 1.5}
+            />
+          </Canvas>
+        )
+      }
     </Box>
   );
 }
